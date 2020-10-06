791fc50d31661859089acc3aa9f599f35346a2f4
khorramk <khorramk.kbsk@gmail.com>
2020-10-06 23:46:03 +0100

    first commit

---
+++ resources/views/carsTable.blade.php
@@ -0,0 +1,15 @@
+<table>
+    <thead>
+        <tr>
+            <th>Make</th>
+            <th>Model</th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
