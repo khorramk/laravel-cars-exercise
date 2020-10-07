3252bf32a72906ce1acf052418c3ee702ea91508
khorramk <khorramk.kbsk@gmail.com>
2020-10-07 16:00:09 +0100

    template to test cars data controller

--- resources/views/carsTable.blade.php
+++ resources/views/carsTable.blade.php
@@ -1,15 +1,42 @@
-<table>
+<table class="table">
     <thead>
         <tr>
-            <th>Make</th>
-            <th>Model</th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th scope="col">Make</th>
+            <th scope="col">Model</th>
+            <th scope="col">Colour</th>
+            <th scope="col">Top speed</th>
+            <th scope="col">view</th>
+            <th scope="col">edit</th>
+            <th scope="col">delete</th>
         </tr>
     </thead>
     <tbody>
+        {{-- {{dd($cars[0]['make'])}} --}}
+        @for ($i = 0; $i < count($cars); $i++)
+        <tr>
+            <td>
+                {{ $cars[$i]['make'] }}
+            </td>
+            <td>
+                {{$cars[$i]['model']}}
+            </td>
+            <td>
+                {{$cars[$i]['colours']}}
+            </td>
+            <td>
+                {{$cars[$i]['topSpeed']}}
+            </td>
+            <td>
+                <a href="#"> view </a>
+            </td>
+            <td>
+                <a href="edit"> edit<a>
+            </td>
+            <td>
+                <a href="#">delete</a>
+            </td>
+        </tr>
+        @endfor
+
     </tbody>
 </table>
